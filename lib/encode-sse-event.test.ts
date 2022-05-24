7c0b4a49c48328d9366fb33f89d5693dcab8e902
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-24 20:14:07 +0200

    add snapshot testing for single property encoding

--- lib/encode-sse-event.test.ts
+++ lib/encode-sse-event.test.ts
@@ -1,8 +1,29 @@
-import { assertThrows, describe, it } from "../dependencies/_testing.std.ts";
+import {
+  assertSnapshot,
+  assertThrows,
+  describe,
+  it,
+} from "../dependencies/_testing.std.ts";
 import { encodeSSEEvent } from "./encode-sse-event.ts";
 
 describe(`encode-sse-event`, () => {
   it(`should throw on empty SSEEvent argument`, () => {
     assertThrows(() => encodeSSEEvent({}));
+  });
+  it(`should throw on a 'retry'-property-only SSEEvent argument`, () => {
+    assertThrows(() => encodeSSEEvent({ retry: 5000 }));
+  });
+
+  it(`should encode single properties`, async (t) => {
+    await assertSnapshot(t, encodeSSEEvent({ id: "UID5346324874238475" }));
+    await assertSnapshot(t, encodeSSEEvent({ name: "custom-event" }));
+    await assertSnapshot(
+      t,
+      encodeSSEEvent({ data: "Some simple string data" }),
+    );
+    await assertSnapshot(
+      t,
+      encodeSSEEvent({ name: "custom-event", retry: 5000 }),
+    );
   });
 });
