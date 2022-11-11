af2f11878ee2d8e0cf4cbecce791aa537639308d
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-11 10:05:37 +0100

    upgrade std_log and std_testing and rename files

--- dependencies/_testing.std.ts
+++
@@ -1,3 +0,0 @@
-export * from "https://deno.land/std@0.140.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.140.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.140.0/testing/snapshot.ts";
